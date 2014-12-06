965b22f5eddba976c289eb4aaa5160ff461451ac
WhatIsThisImNotGoodWithComputers <ohgod@whatisthisimnotgoodwithcomputers.com>
2014-12-06 13:41:26 +0100

    add post region to database with help from 4chan x contributors

--- Extra Flags for int.user.js
+++ Extra Flags for int.user.js
@@ -6,9 +6,19 @@
 // @exclude     http*://boards.4chan.org/int/catalog
 // @exclude     http*://boards.4chan.org/sp/catalog
 // @version     1
-// @grant       none
+// @grant       GM_xmlhttpRequest
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.js
 // ==/UserScript==
+
+// ===============SETTINGS SECTION==================
+//configure your region here
+//NOTE: this will be removed in next versions, we are implementing a database which will check for you're location.
+var region = "Noord-Brabant";
+
+//================END OF SETTINGS===================
+//Don't edit below this line if you don't know what you're doing
+//==================================================
+
 
 var allPostsOnPage = document.getElementsByClassName('post reply');
 
@@ -35,12 +45,25 @@ for (var i=0, max=allPostsOnPage.length; i<max; i++) {
 }
 
 //send flag to system
-/*
-var post_form = $('form');
-
-$("Submit").click(function(){
-  $("p").hide("slow",function(){
-    alert("The paragraph is now hidden");
-  });
-});
-*/
+document.addEventListener('QRPostSuccessful', function(e) {
+  console.log(e.detail.boardID);  // board name    (string)
+  console.log(e.detail.threadID); // thread number (integer in ccd0, string in loadletter)
+  console.log(e.detail.postID);   // post number   (integer in ccd0, string in loadletter)
+  
+  //greasemonkey http request
+  GM_xmlhttpRequest ( {
+    method:     "POST",
+    url:        "http://flaghunters.x10host.com/post_flag.php",
+    data:       "post_nr=" + encodeURIComponent (e.detail.postID)
+                + "&" + "board=" + encodeURIComponent (e.detail.boardID)
+                + "&" + "region=" + encodeURIComponent (region)
+                ,
+    headers:    {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    onload:     function (response) {
+        console.log (response.responseText);
+    }
+} );
+  
+}, false);
