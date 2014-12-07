85ae4fd7dc38525d7e3ce06fda0c7d629725e244
WhatIsThisImNotGoodWithComputers <ohgod@whatisthisimnotgoodwithcomputers.com>
2014-12-07 03:04:52 +0100

    add request posts functionality, started on 4chan inline qr submitting

--- Extra Flags for int.user.js
+++ Extra Flags for int.user.js
@@ -7,10 +7,12 @@
 // @exclude     http*://boards.4chan.org/sp/catalog
 // @version     1
 // @grant       GM_xmlhttpRequest
+// @run-at      document-end
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.js
 // ==/UserScript==
 
 // ===============SETTINGS SECTION==================
+
 //configure your region here
 //NOTE: this will be removed in next versions, we are implementing a database which will check for you're location.
 var region = "Noord-Brabant";
@@ -19,51 +21,96 @@ var region = "Noord-Brabant";
 //Don't edit below this line if you don't know what you're doing
 //==================================================
 
-
-var allPostsOnPage = document.getElementsByClassName('post reply');
+var allPostsOnPage = document.getElementsByClassName('postContainer');
+var postNrs = new Array();
 
 for (var i=0, max=allPostsOnPage.length; i<max; i++) {
-  
-  var nameBlock = allPostsOnPage[i].getElementsByClassName('postInfo desktop')[0].getElementsByClassName('nameBlock')[0];
-  var name = nameBlock.getElementsByClassName('name')[0];
-  
-  if (name.innerHTML === 'Friesland') {
-    var fleg = $(name).first().next();
-    var newFleg = fleg.clone().attr("title", "Friesland").appendTo(nameBlock);
-    newFleg[0].innerHTML = "<img src='https://raw.githubusercontent.com/flaghunters/Extra-Flags-for-int-/master/flegs/Netherlands/Friesland.png'>";
-    newFleg[0].className = newFleg[0].className = "noClass";
-    //padding format: TOP x RIGHT_OF x BOTTOM x LEFT_OF
-    newFleg[0].style = "padding: 0px 0px 0px 5px; vertical-align:;display: inline-block; width: 16px; height: 11px; position: relative; top: 1px;"
-  } else if (name.innerHTML === 'Noord-Brabant') {
-    var fleg = $(name).first().next();
-    var newFleg = fleg.clone().attr("title", "Noord-Brabant").appendTo(nameBlock);
-    newFleg[0].innerHTML = "<img src='https://raw.githubusercontent.com/flaghunters/Extra-Flags-for-int-/master/flegs/Netherlands/Noord-Brabant.png'>";
-    newFleg[0].className = newFleg[0].className = "noClass";
-    //padding format: TOP x RIGHT_OF x BOTTOM x LEFT_OF
-    newFleg[0].style = "padding: 0px 0px 0px 5px; vertical-align:;display: inline-block; width: 16px; height: 11px; position: relative; top: 1px;"
-  }
-}
+	
+	var tempPostNr = allPostsOnPage[i].id;
+	tempPostNr = tempPostNr.replace("pc","");
+	postNrs.push(tempPostNr);
+}	
+//greasemonkey http request
+GM_xmlhttpRequest ( {
+method:     "POST",
+url:        "http://flaghunters.x10host.com/get_flags.php",
+data:       "post_nrs=" + encodeURIComponent (postNrs)
+            //+ "&" + "board=" + encodeURIComponent (e.detail.boardID)
+            //+ "&" + "region=" + encodeURIComponent (region)
+            ,
+headers:    {
+    "Content-Type": "application/x-www-form-urlencoded"
+	},
+	onload:     function (response) {
+    //parse returned data
+	var jsonData = JSON.parse(response.responseText);
 
-//send flag to system
-document.addEventListener('QRPostSuccessful', function(e) {
-  console.log(e.detail.boardID);  // board name    (string)
-  console.log(e.detail.threadID); // thread number (integer in ccd0, string in loadletter)
-  console.log(e.detail.postID);   // post number   (integer in ccd0, string in loadletter)
-  
-  //greasemonkey http request
-  GM_xmlhttpRequest ( {
-    method:     "POST",
-    url:        "http://flaghunters.x10host.com/post_flag.php",
-    data:       "post_nr=" + encodeURIComponent (e.detail.postID)
-                + "&" + "board=" + encodeURIComponent (e.detail.boardID)
-                + "&" + "region=" + encodeURIComponent (region)
-                ,
-    headers:    {
-        "Content-Type": "application/x-www-form-urlencoded"
-    },
-    onload:     function (response) {
-        console.log (response.responseText);
-    }
+	for (var i = 0; i < jsonData.length; i++) {
+		var post = jsonData[i];
+		var postToAddFlagTo = document.getElementById("pc" + post.post_nr);
+		var nameBlock = postToAddFlagTo.getElementsByClassName('post reply')[0]
+			.getElementsByClassName('postInfo desktop')[0].getElementsByClassName('nameBlock')[0];
+		var name = nameBlock.getElementsByClassName('name')[0];
+		
+		//for some reason the clone() up ahead needs the first flag and
+		//the title for the flag link needs to get it by class
+		var currentFlag = $(name).first().next();
+		var currentFlagForTitle = nameBlock.getElementsByClassName('flag')[0];
+
+		var newFlag = currentFlag.clone().attr("title", post.region).appendTo(nameBlock);
+		
+		newFlag[0].innerHTML = "<img src='https://raw.githubusercontent.com/flaghunters/Extra-Flags-for-int-/master/flegs/" + currentFlagForTitle.title + "/" + post.region + ".png'>";
+		newFlag[0].className = newFlag[0].className = "noClass";
+		//padding format: TOP x RIGHT_OF x BOTTOM x LEFT_OF
+		newFlag[0].style = "padding: 0px 0px 0px 5px; vertical-align:;display: inline-block; width: 16px; height: 11px; position: relative; top: 1px;"
+	}
+	}
 } );
-  
+
+//send flag to system on 4chan x (v2, loadletter, v3 untested) post
+document.addEventListener('QRPostSuccessful', function(e) {
+	//handy comment to save by ccd0
+  //console.log(e.detail.boardID);  // board name    (string)
+  //console.log(e.detail.threadID); // thread number (integer in ccd0, string in loadletter)
+  //console.log(e.detail.postID);   // post number   (integer in ccd0, string in loadletter)
+
+	//greasemonkey http request
+	GM_xmlhttpRequest ( {
+		method:     "POST",
+		url:        "http://flaghunters.x10host.com/post_flag.php",
+		data:       "post_nr=" + encodeURIComponent (e.detail.postID)
+					+ "&" + "board=" + encodeURIComponent (e.detail.boardID)
+					+ "&" + "region=" + encodeURIComponent (region)
+					,
+		headers:    {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		onload:     function (response) {
+			console.log(response.responseText);
+		}
+	} );
 }, false);
+
+//send flag to system on 4chan inline post
+//only works on int for now! need to parse board from somewhere
+document.addEventListener('4chanQRPostSuccess', function(e) {
+	
+    console.log("4chanQRPostSuccess");
+	  
+	//greasemonkey http request
+	GM_xmlhttpRequest ( {
+		method:     "POST",
+		url:        "http://flaghunters.x10host.com/post_flag.php",
+		data:       "post_nr=" + encodeURIComponent (e.detail.postId)
+					+ "&" + "board=" + encodeURIComponent ("int")
+					+ "&" + "region=" + encodeURIComponent (region)
+					,
+		headers:    {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		onload:     function (response) {
+			console.log(response.responseText);
+		}
+	} );
+}, false);
+
