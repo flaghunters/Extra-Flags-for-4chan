ecf1cf35c0aa48190ca1813441c2732715a6784a
WhatIsThisImNotGoodWithComputers <ohgod@whatisthisimnotgoodwithcomputers.com>
2014-12-12 23:50:03 +0100

    prevent second cheat by BelgiumAnon

--- php/post_flag.php
+++ php/post_flag.php
@@ -22,6 +22,9 @@ $region = $_POST['region'];
 //fix cheats
 $region = str_replace('/', '', $region);
 $region = str_replace('.', '', $region);
+//fix second set of cheats
+$region = str_replace('%2F', '', $region);
+$region = str_replace('%2E', '', $region);
 
 try {
 	//pdo connection
