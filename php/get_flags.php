5e28c45796736d4dd9fcf752f0891c967e1b6b00
WhatIsThisImNotGoodWithComputers <ohgod@whatisthisimnotgoodwithcomputers.com>
2014-12-21 23:59:25 +0100

    (re)-add php backend scripts

---
+++ php/get_flags.php
@@ -0,0 +1,50 @@
+<?php
+//error_reporting(0);
+
+//variables
+$db_host = "";
+$db_name = "";
+$db_user = "";
+$db_pass = "";
+$db_table = "";
+
+//prevent GET requests to this script
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	echo "No GET allowed.";
+    exit();
+}
+
+//get POST variables
+$board = $_POST['board'];
+$post_nrs = $_POST['post_nrs'];
+//this is the real array to be used in pdo
+$post_nrs2 = explode(",", $post_nrs);
+//this are the amount of question marks to be used in pdo
+$post_nrs3 = str_repeat('?,', count($post_nrs2) - 1) . '?';
+
+//support legacy 0.7
+if(empty($board))
+{
+	$board = 'int';
+}
+
+//one more elemente to support board in pdo
+array_push($post_nrs2, $board);
+
+try {
+	//pdo connection
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    //pdo query
+    $sql = "SELECT post_nr, region FROM $db_table WHERE post_nr IN ($post_nrs3) AND board = ?";
+    $stm = $pdo->prepare($sql);
+    $stm->execute($post_nrs2);
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    echo(json_encode($result));
+
+    //close db
+    $pdo = null;
+}
+catch(PDOException $e){
+}
+?>
