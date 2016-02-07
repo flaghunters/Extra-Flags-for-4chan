42d6a430961e592aad1768b38655773f972648bb
WhatIsThisImNotGoodWithComputers <ohgod@whatisthisimnotgoodwithcomputers.com>
2016-02-07 22:40:06 +0100

    forgot to bump version to 0.13 for chrome fix

--- Extra Flags for int.user.js
+++ Extra Flags for int.user.js
@@ -8,7 +8,7 @@
 // @exclude     http*://boards.4chan.org/int/catalog
 // @exclude     http*://boards.4chan.org/sp/catalog
 // @exclude     http*://boards.4chan.org/pol/catalog
-// @version     0.12
+// @version     0.13
 // @grant       GM_xmlhttpRequest
 // @grant       GM_registerMenuCommand
 // @grant       GM_getValue
