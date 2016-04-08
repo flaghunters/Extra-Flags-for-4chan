99a841a38ce2844b1a309c89f6a195db91f29df1
WhatIsThisImNotGoodWithComputers <ohgod@whatisthisimnotgoodwithcomputers.com>
2016-04-09 00:25:41 +0200

    add gloss script

---
+++ php/glossify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Depends on imagemagicks composite
+# Made by ohgod AT whatisthisimnotgoodwithcomputers DOT com
+
+GLOSS_TEMPLATE="Gloss Template 3 (Use this one).png"
+
+for flag in gloss/*;
+do
+	echo -e "Glossing: \033[05;44;33m$flag\033[0m..."
+	$(composite "$GLOSS_TEMPLATE" "$flag" "$flag")
+	echo "Done"
+done
+
+echo -e "Running \033[05;44;33moptipng\033[0m..."
+$(optipng -o7 gloss/*)
+
+exit 0
