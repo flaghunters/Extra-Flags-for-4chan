30fbccffb869e3b5502fcafd7b8fcc26cb24201e
WhatIsThisImNotGoodWithComputers <ohgod@whatisthisimnotgoodwithcomputers.com>
2016-05-01 21:21:40 +0200

    finalize gen flag list script

--- beta/php/generate_flag_list.sh
+++ beta/php/generate_flag_list.sh
@@ -16,13 +16,14 @@ sed -i '/'${FILE}'/d' "${FLAG_TOP_DIR_FILE}"
 #generate regions recursive
 #feed this method full (relative) path
 gen_list()
-{
-	#echo "${1}"
-	
+{	
 	#gen list for folder I'm in
 	ls -p "${1}" | grep -v / > "${1}${FILE}"
 	sed -i '/'${FILE}'/d' "${1}${FILE}"
 	sed -i 's/.png//g' "${1}${FILE}"
+	
+	#add copy to big list
+	cat "${1}${FILE}" >> "${FILE}"
 	
 	#run recursive for folders
 	FOLDER_LIST=''
@@ -32,14 +33,16 @@ gen_list()
 	shopt -u nullglob
 	
 	if [ ! -z "${FOLDER_LIST}" ]; then
-		#echo 11111111111111111111111111111111111 ${FOLDER_LIST}
 		for region in "${FOLDER_LIST[@]}"
 		do
-			#echo "${region}"
 			gen_list "${region}"
 		done
 	fi
 }
+
+#remove big list
+rm "${FILE}"
+touch "${FILE}"
 
 #kick off recursive generation
 while IFS='' read -r country || [[ -n "${country}" ]]; do
