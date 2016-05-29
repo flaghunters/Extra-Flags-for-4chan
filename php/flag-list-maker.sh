9d26413f5421aef35d8db3371c2b47ac96e90109
kek@mememachine <sno.kattunge@gmail.com>
2016-05-29 13:20:47 -0400

    Add Florida flags and modify flagscript to list correctly using rev and cut

--- php/flag-list-maker.sh
+++ php/flag-list-maker.sh
@@ -2,7 +2,7 @@ ls -1d -- */ | sed 's/\/$//' | sed 's/\ /\\\ /g'> .dir.txt
 while read directory
 do
 	cd "$PWD/$directory";
-	ls -p1 | grep -v / | sed -e 's/\..*$//' | grep -v "flag_list" > flag_list.txt;
+	ls -a | rev | cut -c5- | rev | grep -v "flag_list" | sed '/^$/d' > flag_list.txt;
 	cd ".."
 done < .dir.txt
 	
